c230fb3cb2c478d41ddd98fb2910a900c67f60ba
Vlad Plescan <vplescan@deloittece.com>
2023-07-31 16:34:25 +0300

    Adding response status check test

--- src/test/java/starter/AutomationTests/StepsDefinition.java
+++ src/test/java/starter/AutomationTests/StepsDefinition.java
@@ -1,23 +1,31 @@
 package starter.AutomationTests;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.specification.RequestSpecification;
 import net.serenitybdd.rest.SerenityRest;
+import org.junit.Assert;
 
 public class StepsDefinition {
 
     public RequestSpecification rq;
 
-@Given("^the url: (.*)$")
-    public void setURL (String url) {
-    rq = SerenityRest.given().baseUri(url).log().all();
+    @Given("^the url: (.*)$")
+    public void setURL(String url) {
+        rq = SerenityRest.given().baseUri(url).log().all();
 
-}
+    }
 
-@When("^I perform GET call to it$")
-    public void performGet () {
-    rq.get().prettyPrint();
-}
+    @When("^I perform GET call to it$")
+    public void performGet() {
+        rq.get().prettyPrint();
+    }
+
+    @Then("^the response should be (.*)$")
+    public void responseCode(Integer statusCode) {
+        Integer actualStatusCode = SerenityRest.lastResponse().getStatusCode();
+        Assert.assertEquals(statusCode,actualStatusCode);
+    }
 
 }
--- src/test/resources/features/Not-Login.feature
+++ src/test/resources/features/Not-Login.feature
@@ -3,4 +3,4 @@ Feature: SimpleBookAPI endpoints that require NotLoggedIn
   Scenario: Perform a call to health check
     Given the url: https://simple-books-api.glitch.me
     When I perform GET call to it
-#    Then the response should be 200
+    Then the response should be 200
