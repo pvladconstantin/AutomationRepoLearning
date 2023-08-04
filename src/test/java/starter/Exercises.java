b92081462fe08035d83dee11ff661104eabddd5a
Vlad Plescan <vplescan@deloittece.com>
2023-08-05 02:07:48 +0300

    Practicing for and do-while.

--- src/test/java/starter/Exercises.java
+++
@@ -1,9 +0,0 @@
-package starter;
-
-public class Exercises {
-    public static void main(String[] args) throws Exception { boolean flag = true;
-        if (flag) {
-            System.out.println("true");
-        }
-        System.out.println("false");
-    } }
