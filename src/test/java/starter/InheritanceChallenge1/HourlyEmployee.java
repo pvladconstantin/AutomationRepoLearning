0b6c71ba350c15b0b4e57553ca7df70da2c3cc3e
Vlad Plescan <vplescan@deloittece.com>
2023-08-04 10:55:59 +0300

    Adding Inheritance course Challenge exercise.

---
+++ src/test/java/starter/InheritanceChallenge1/HourlyEmployee.java
@@ -0,0 +1,19 @@
+package starter.InheritanceChallenge1;
+
+public class HourlyEmployee extends Employee {
+    private double hourlyPayRate;
+
+    public HourlyEmployee(String name, String birthDay, String hireDate, double hourlyPayRate) {
+        super(name, birthDay, hireDate);
+        this.hourlyPayRate = hourlyPayRate;
+    }
+
+    @Override
+    public double collectPay() {
+        return 40 * hourlyPayRate;
+    }
+
+    public double getDoublePay() {
+        return 2 * collectPay();
+    }
+}
