e75387c4f1ce47afdeccebbb5ab4cc2dc65d52d1
Vlad Plescan <vplescan@deloittece.com>
2023-08-08 12:22:01 +0300

    Practicing Class and Methods.

---
+++ src/test/java/starter/ClassAndMethods/CoursePractice/Main.java
@@ -0,0 +1,11 @@
+package starter.ClassAndMethods.CoursePractice;
+
+public class Main {
+    public void fullThrottle() {
+        System.out.println("The car is going as fast as it can!");
+    }
+
+    public void speed(int maxSpeed) {
+        System.out.println("Max speed is: " + maxSpeed);
+    }
+}
---
+++ src/test/java/starter/ClassAndMethods/CoursePractice/Second.java
@@ -0,0 +1,9 @@
+package starter.ClassAndMethods.CoursePractice;
+
+public class Second {
+    public static void main(String[] args) {
+        Main myCar = new Main();     // Create a myCar object
+        myCar.fullThrottle();      // Call the fullThrottle() method
+        myCar.speed(200);          // Call the speed() method
+    }
+}
